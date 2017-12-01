42aecff2ca956e7c70e8aa792224e9300115e3d0
Johan van Rhyn <github@jvanrhyn.co.za>
2017-12-01 16:55:54 +0200

    Working prod build

--- src/Easy-Dash/webpack.config.js
+++ src/Easy-Dash/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 const { AureliaPlugin } = require('aurelia-webpack-plugin');
+const UglifyEsPlugin = require('uglify-es-webpack-plugin');
 const bundleOutputDir = './wwwroot/dist';
 
 module.exports = (env) => {
+    console.warn('env =>', env);
     const isDevBuild = !(env && env.prod);
     return [{
         stats: { modules: false },
@@ -31,14 +33,14 @@ module.exports = (env) => {
                 context: __dirname,
                 manifest: require('./wwwroot/dist/vendor-manifest.json')
             }),
-            new AureliaPlugin({ aureliaApp: 'boot' })
+            new AureliaPlugin({ aureliaApp: 'boot' }),
         ].concat(isDevBuild ? [
             new webpack.SourceMapDevToolPlugin({
                 filename: '[file].map', // Remove this line if you prefer inline source maps
                 moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]')  // Point sourcemap entries to the original file locations on disk
             })
         ] : [
-            new webpack.optimize.UglifyJsPlugin()
+            new UglifyEsPlugin()
         ])
     }];
 }
