978909f185b630dc3aae71d8833a1e2aab00e126
Heiko Light <mail@heikolight.com>
2017-12-04 18:53:36 +0200

    Add initial Failed widget to Dashboard

---
+++ src/Easy-Dash/ClientApp/app/components/dashboard-failed/dashboard-failed.ts
@@ -0,0 +1,147 @@
+﻿import { HttpClient } from 'aurelia-fetch-client';
+import { inject, PLATFORM } from 'aurelia-framework';
+import * as moment from 'moment';
+import { HubConnection } from '@aspnet/signalr-client';
+import { DashboardResult } from '../models/dashboardresult';
+import { TestSummary } from '../models/testsummary';
+import { Busy } from '../busy/busy';
+import { DialogService } from 'aurelia-dialog';
+
+@inject(HttpClient, DialogService, Busy)
+export class DashboardFailed {
+    dashboardResults: DashboardResult[];
+    private hubConnection: HubConnection;
+
+    constructor(public http: HttpClient, public dialogService: DialogService, private busy: Busy) {
+        this.loadDashboardResults();
+    }
+
+    async activate(): Promise<void> {
+        try {
+            this.busy.on();
+            return await this.hookup();
+        } catch (e) {
+
+        } finally {
+            this.busy.off();
+        }
+    }
+
+    deactivate() {
+        if (this.hubConnection) {
+            this.hubConnection.stop();
+        }
+    }
+
+    private async hookup() {
+        try {
+            this.hubConnection = new HubConnection('/dashboardsignal');
+            await this.hubConnection.start();
+
+            this.hubConnection.on('TestStarted',
+                (id: number) => {
+                    let row = this.dashboardResults.findIndex(result => result.id === id);
+                    if (row >= 0) {
+                        this.dashboardResults[row].lastStatus = 'Running';
+                    }
+                });
+
+            this.hubConnection.on('TestEnded',
+                (result: DashboardResult) => {
+                    let row = this.dashboardResults.findIndex(item => item.id === result.id);
+                    if (row >= 0) {
+                        const item = this.dashboardResults[row];
+                        item.nextUpdate = result.nextUpdate;
+                        item.lastStatus = result.lastStatus;
+                        item.description = result.description;
+                        item.lastUpdate = result.lastUpdate;
+
+                        setTimeout(() => this.sortResults(), 5000);
+                    }
+                });
+
+            this.hubConnection.on('ConfigModified',
+                (id: number) => {
+                    this.addOrUpdateDashboardResult(id);
+                });
+
+            return;
+
+        } catch (e) {
+            console.warn('Exception on Init', e);
+        }
+    }
+
+    sortResults() {
+        this.dashboardResults.sort((a, b) => {
+            return new Date(a.nextUpdate).getTime() - new Date(b.nextUpdate).getTime();
+        });
+    }
+
+    private async loadDashboardResults() {
+        try {
+            this.busy.on();
+            const result = await this.http.fetch('api/Dashboard/Results');
+
+            const resultdata = await result.json() as DashboardResult[];
+            this.dashboardResults = resultdata.filter(r => r.lastStatus === "Fail").map(m => {
+                const item = new DashboardResult();
+
+                item.lastUpdate = m.lastUpdate;
+                item.description = m.description;
+                item.id = m.id;
+                item.lastStatus = m.lastStatus;
+                item.nextUpdate = m.nextUpdate;
+
+                return item;
+            });
+
+            this.dashboardResults.forEach((item) => {
+                this.sortResults();
+            });
+        } catch (error) {
+            console.error(error);
+        } finally {
+            this.busy.off();
+        }
+    }
+
+    async detailsClick(id: number) {
+        try {
+            this.busy.on();
+            const response = await this.http.fetch(`api/Dashboard/${id}/details`);
+            const summaries = await response.json() as TestSummary[];
+
+            this.busy.off();
+
+            this.dialogService.open({ viewModel: PLATFORM.moduleName('app/components/dashboard-details/dashboard-details'), model: summaries, lock: false })
+                .whenClosed(response => {
+                    if (response.wasCancelled) {
+                        return;
+                    }
+                });
+        } catch (e) {
+            console.error(e);
+        } finally {
+            this.busy.off();
+        }
+    }
+
+    private async addOrUpdateDashboardResult(id: number) {
+        try {
+            const response = await this.http.fetch('api/Dashboard/Find/' + id);
+            const result = await response.json() as DashboardResult;
+
+            const existingIndex = this.dashboardResults.findIndex(x => x.id === id);
+            if (existingIndex > -1) {
+                this.dashboardResults.splice(existingIndex, 1);
+            }
+
+            if (result && result.lastStatus === "Fail") {
+                this.dashboardResults.push(result);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
